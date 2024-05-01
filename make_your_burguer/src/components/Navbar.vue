170e3328b50c2f5a9ed1eca98b30084789b1fe3d
Arllain <arllain@gmail.com>
2024-05-01 10:57:19 -0300

    Implementando o Vue Router

---
+++ make_your_burguer/src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div id="nav">
+            <router-link to="/"> 
+                <img src="" alt="" id="logo">
+        </router-link>
+        <routerLink to="/">Home</routerLink>
+        <routerLink to="/pedidos">Pedidos</routerLink>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NavBar",
+        components: {
+        }
+    }
+</script>
+
+<style>
+
+</style>
