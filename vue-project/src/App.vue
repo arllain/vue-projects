35f793e102dea852a442891080f9c1c8454aae11
Arllain <arllain@gmail.com>
2024-06-02 17:30:14 -0300

    vue components

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,9 +1,64 @@
-<script setup></script>
-
 <template>
-  <header>
-    <div>Hello, Vue !</div>
-  </header>
+  <BlogPost
+    id="1"
+    blogPostTitle="What is a muggle?"
+    blogPostContent="A muggle is a person who lacks any sort of magical ability ..."
+  />
+
+  <hr />
+  <BlogPost :id="blogPostId" :blogPostTitle="blogPostTitle" :blogPostContent="blogPostContent" />
+
+  <!-- <hr />
+  <BlogPost
+    v-for="post in posts"
+    :key="post.id"
+    :id="post.id"
+    :blogPostTitle="post.blogPostTitle"
+    :blogPostContent="post.blogPostContent"
+  ></BlogPost> -->
+
+  <hr />
+  <BlogPost v-for="post in posts" :key="post.id" v-bind="post"></BlogPost>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import BlogPost from './components/BlogPost.vue'
+
+let blogPostId = ref(2)
+let blogPostTitle = ref(
+  'Harry Potter and the Cursed Child Bradway production suspended until April'
+)
+let blogPostContent = ref(
+  'Adding to teh earlier post, the Broadway production of Herry Potter and the Cursed Child has been ...'
+)
+
+let posts = ref([
+  {
+    id: 1,
+    blogPostTitle: 'Harry Potter and the Cursed Child Bradway production suspended until April',
+    blogPostContent:
+      'Adding to teh earlier post, the Broadway production of Herry Potter and the Cursed Child has been ...'
+  },
+  {
+    id: 2,
+    blogPostTitle: 'Harry Potter and the Cursed Child Bradway production suspended until April',
+    blogPostContent:
+      'Adding to teh earlier post, the Broadway production of Herry Potter and the Cursed Child has been ...'
+  },
+  {
+    id: 3,
+    blogPostTitle: 'Harry Potter and the Cursed Child Bradway production suspended until April',
+    blogPostContent:
+      'Adding to teh earlier post, the Broadway production of Herry Potter and the Cursed Child has been ...'
+  },
+  {
+    id: 4,
+    blogPostTitle: 'Harry Potter and the Cursed Child Bradway production suspended until April',
+    blogPostContent:
+      'Adding to teh earlier post, the Broadway production of Herry Potter and the Cursed Child has been ...'
+  }
+])
+</script>
 
 <style scoped></style>
---
+++ vue-project/src/components/BlogPost.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="blog-post">
+    <p>{{ message }}</p>
+    <h2>{{ id }} - {{ blogPostTitle }}</h2>
+    <h4>{{ blogPostContent }}</h4>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+let message = ref('This is the BlogPost component')
+
+// defineProps({ id: Number, blogPostTitle: String, blogPostContent: String })
+let props = defineProps(['id', 'blogPostTitle', 'blogPostContent'])
+console.log(props.blogPostTitle)
+</script>
+
+<style scoped>
+.blog-post {
+  background-color: aqua;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+</style>
